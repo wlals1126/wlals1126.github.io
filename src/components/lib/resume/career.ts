a76184346b7336685be5ec1606ec9008d5202e2c
유지민 <a01040987587@gmail.com>
2025-01-27 21:40:02 +0900

    chore: 업무 경험 수정

--- src/components/lib/resume/career.ts
+++ src/components/lib/resume/career.ts
@@ -1,22 +1,20 @@
 export type Career = {
 	companyName: string
-	beginAt: `${number}-${number}`
-	endAt: `${number}-${number}`
+	beginAt: `${number}.${number}`
+	endAt: `${number}.${number}`
 	team: Team[]
 }
 
 export type Team = {
 	teamName: string
 	position: string
-	beginAt: `${number}-${number}`
-	endAt?: `${number}-${number}`
+	beginAt: `${number}.${number}`
+	endAt?: `${number}.${number}`
 	projects: Project[]
 }
 
 export type Project = {
 	title: string
-	beginAt: `${number}-${number}`
-	endAt?: `${number}-${number}`
 	description: string
 	role: Works[]
 	works: Works[]
@@ -52,19 +50,17 @@ export type TechStack =
 export const CAREER: Career[] = [
 	{
 		companyName: '한국신용데이터',
-		beginAt: '2024-10',
-		endAt: '2025-01',
+		beginAt: '2024.10',
+		endAt: '2025.01',
 		team: [
 			{
 				teamName: '데이터플랫폼팀',
 				position: 'Frontend Engineer',
-				beginAt: '2024-10',
-				endAt: '2025-01',
+				beginAt: '2024.10',
+				endAt: '2025.01',
 				projects: [
 					{
 						title: '액션 카탈로그',
-						beginAt: '2024-10',
-						endAt: '2025-01',
 						description:
 							'AI 응답 결과를 개선하기 위해 다양한 데이터 소스에 효율적으로 접근할 수 있는 시스템입니다. 통합 어드민 환경에서 프롬프트 최적화 및 사전 정의된 시나리오 구축을 통해 AI 성능과 정확도를 향상시키며, 필요한 API 리스트를 관리 및 제공합니다.',
 						role: [
@@ -93,8 +89,6 @@ export const CAREER: Career[] = [
 					},
 					{
 						title: 'AI 에이전트',
-						beginAt: '2024-10',
-						endAt: '2025-01',
 						description:
 							'AI-Agent는 사용자의 질문을 이해하고 적절한 답변을 제공하기 위해 설계된 대화형 시스템으로, 다양한 시나리오를 기반으로 사용자 의도를 분석하고 백엔드와 연동하여 데이터를 검색하고 처리하는 자동화된 기능을 구현한 프로젝트입니다.',
 						role: [
@@ -109,9 +103,9 @@ export const CAREER: Career[] = [
 								description: '백엔드',
 								subDescription: [
 									'AI 비서 API 설계 및 기능 개선',
+									'대화 저장 로직 개선 및 DB 테이블 재설계',
 									'AI 비서 기능 개선을 위한 엔드포인트 설계 및 구축',
-									'텍스트 기반 질의응답 처리 (Text-to-SQL) 및 부가정보 리턴 로직 개발',
-									'프롬프트 최적화 및 서버 로깅 작업'
+									'텍스트 기반 질의응답 처리 (Text-to-SQL) 및 부가정보 리턴 로직 개발'
 								]
 							}
 						],
